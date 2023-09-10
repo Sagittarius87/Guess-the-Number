a0565493188ba4d23dd677b0f86ed427236f7882
Sagittarius87 <Jarik-87@yandex.ru>
2023-09-10 16:59:38 +0300

    Добавил переменные running, ctx, sprites, sounds. Добавил метод init()

--- script/game.js
+++ script/game.js
@@ -1,7 +1,16 @@
-game = {
+let game = {
+    running: true,
+    ctx: null,
+    sprites: {},
+    sounds: {},
+    init() {
+        this.ctx = document.getElementById('idcanvas').getContext('2d')
+        console.log(this.ctx)
+    },
     start() {
         console.log('start game')
     }
 }
 
 game.start()
+game.init()
