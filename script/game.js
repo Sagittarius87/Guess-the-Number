f5c7e9d8b9cc391a2e32e4116a5e66b442d9126f
Sagittarius87 <Jarik-87@yandex.ru>
2023-09-10 23:47:24 +0300

    Сделал инициализацию 2D контекста. Добавил для обьекта game свойства enteredNumber, hiddenNumber, messages. Добавил свойства для вложенных объектов sprites и sounds

--- script/game.js
+++ script/game.js
@@ -1,11 +1,41 @@
 let game = {
     running: true,
     ctx: null,
-    sprites: {},
-    sounds: {},
+    enteredNumber: null,
+    hiddenNumber: null,
+    messages: ['0'],
+    sprites: {
+        logo: null,
+        mainImage1: null,
+        mainImage2: null,
+        mainImage3: null,
+        buttonNewGame: null,
+        labelEnteredNumber: null,
+        labelMessage: null,
+        buttonZero: null,
+        buttonOne: null,
+        buttonTwo: null,
+        buttonThree: null,
+        buttonFour: null,
+        buttonFive: null,
+        buttonSix: null,
+        buttonSeven: null,
+        buttonEight: null,
+        buttonNine: null,
+        buttonBackspace: null,
+        buttonCheck: null
+    },
+    sounds: {
+        soundClick: null
+    },
     init() {
         this.ctx = document.getElementById('idcanvas').getContext('2d')
-        console.log(this.ctx)
+        console.log('canvas start')
+    },
+    preload() {
+        this.logo = new Image()
+        this.logo.src = '/img/Угодай число.png'
+        console.log(this.logo.src)        
     },
     start() {
         console.log('start game')
@@ -14,3 +44,4 @@ let game = {
 
 game.start()
 game.init()
+game.preload()
